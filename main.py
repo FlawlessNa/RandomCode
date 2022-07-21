3f096295e3b85ec6038834ee7ea7c8b92f28b6e4
Nass <nassim.benchabane.1@gmail.com>
2022-07-20 23:03:16 -0400

    Image recognition framework for future

--- main.py
+++ main.py
@@ -26,13 +26,49 @@ config.read(config.get(section='Login Credentials', option='path'))
 
 if __name__ == '__main__':
 
-    manager = QueueManager(config)
+    test = MageManager(config, 'LegalizeIt', 'bot')
+    init_control_gui()
 
-    proc1 = multiprocessing.Process(target=manager.looter)
-    proc2 = multiprocessing.Process(target=manager.bishop)
-    proc3 = multiprocessing.Process(target=manager.mage, args=('Goldmine1', 'bot', 'second', ))
-    proc4 = multiprocessing.Process(target=manager.mage, args=('Goldmine2', 'top', 'first', ))
-    proc5 = multiprocessing.Process(target=manager.mage, args=('Goldmine3', 'top', 'second', ))
+    while True:
+        loop_time= time.time()
+        sc = test.take_screenshot()
+        processed_sc = apply_hsv_filter(sc)
+
+        cv2.imshow('Processed Img', processed_sc)
+        current_filters = get_hsv_filter_from_controls()
+
+        # images = []
+        # for item in config.items(section='MOB Images'):
+        #     imgs = eval(item[1])
+        #     images.extend(imgs)
+        #
+        # mobs = []
+        # for image in images:
+        #     mobs.append(find_image(sc, cv2.imread(image, cv2.IMREAD_COLOR), threshold=0.7))
+        #
+        # for mob in mobs:
+        #     for rects in mob:
+        #         x, y, w, h = rects
+        #         cv2.rectangle(sc, (x, y), (x + w, y + h), (255, 0, 0), thickness=5)
+
+        # cv2.imshow('Test', sc)
+        print('FPS {}'.format(1 / (time.time() - loop_time)))
+        if cv2.waitKey(1) == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+
+
+
+
+
+    # manager = QueueManager(config)
+    #
+    # proc1 = multiprocessing.Process(target=manager.looter)
+    # proc2 = multiprocessing.Process(target=manager.bishop)
+    # proc3 = multiprocessing.Process(target=manager.mage, args=('Goldmine1', 'bot', 'second', ))
+    # proc4 = multiprocessing.Process(target=manager.mage, args=('Goldmine2', 'top', 'first', ))
+    # proc5 = multiprocessing.Process(target=manager.mage, args=('Goldmine3', 'top', 'second', ))
 
     # proc1 = multiprocessing.Process(target=manager.looter, args=('MidN',))
     # proc2 = multiprocessing.Process(target=manager.bishop, args=('GriZ', 'bot'))
@@ -40,18 +76,18 @@ if __name__ == '__main__':
     # proc4 = multiprocessing.Process(target=manager.mage, args=('ZirG', 'top', 'first', ))
     # proc5 = multiprocessing.Process(target=manager.mage, args=('Leake', 'top', 'second', ))
 
-    print('time started {}'.format(time.strftime('%H:%M:%S', time.localtime())))
-    proc1.start()
-    proc2.start()
-    proc3.start()
-    proc4.start()
-    proc5.start()
-
-    proc1.join()
-    proc2.join()
-    proc3.join()
-    proc4.join()
-    proc5.join()
+    # print('time started {}'.format(time.strftime('%H:%M:%S', time.localtime())))
+    # proc1.start()
+    # proc2.start()
+    # proc3.start()
+    # proc4.start()
+    # proc5.start()
+    #
+    # proc1.join()
+    # proc2.join()
+    # proc3.join()
+    # proc4.join()
+    # proc5.join()
 
 
 
